933e0770421b548d0b79e11489a340d64d64957b
ayoajibola <aji21001@byui.edu>
2021-04-26 11:11:48 +0100

    innitial commit

--- js/assignments.js
+++ js/assignments.js
@@ -1,2 +1,11 @@
-const fulldate = new Date();
- footer.querySelector("currentDate").innerHTML = "Last updated " + fulldate;
+let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday" ,"Thursday", "Friday", "Saturday" ];
+//let day = getDate();
+let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//let month = getMonth();
+//let year = getFullYear();
+let d = new Date();
+
+let myDate = d.getDate() + " " + dayNames[d.getDay()] + " " + monthNames[d.getMonth()] + " " + d.getFullYear();
+
+//let currentDate = dayNames[2] + " " + monthNames[3];
+document.getElementById('currentDate').innerHTML = myDate;
