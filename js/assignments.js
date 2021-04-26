d146eb364253a14a57af3cfb0093ed984bfc6648
ayoajibola <aji21001@byui.edu>
2021-04-26 11:12:58 +0100

    innitial commit

--- js/assignments.js
+++ js/assignments.js
@@ -1,11 +1,37 @@
-let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday" ,"Thursday", "Friday", "Saturday" ];
-//let day = getDate();
-let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-//let month = getMonth();
-//let year = getFullYear();
+let dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 let d = new Date();
 
-let myDate = d.getDate() + " " + dayNames[d.getDay()] + " " + monthNames[d.getMonth()] + " " + d.getFullYear();
+let myDate =
+  d.getDate() +
+  " " +
+  dayNames[d.getDay()] +
+  " " +
+  monthNames[d.getMonth()] +
+  " " +
+  d.getFullYear();
 
 //let currentDate = dayNames[2] + " " + monthNames[3];
-document.getElementById('currentDate').innerHTML = myDate;
+document.getElementById("currentDate").innerHTML = myDate;
