2bef01b8196014059a31a0e79c627f73a922b0f0
ayoajibola <aji21001@byui.edu>
2021-05-02 17:20:37 +0100

    update

--- lesson2/js/designPrinciples.js
+++ lesson2/js/designPrinciples.js
@@ -1,41 +1,40 @@
 let dayNames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  
-  let d = new Date();
-  
-  let myDate =
-    dayNames[d.getDay()] +
-    ", " +
-    monthNames[d.getMonth()] +
-    " " +
-    d.getDate() +
-    " " +
-    d.getFullYear();
-  
-    let year = d.getFullYear();
-  
-  
-  document.querySelector("p").innerHTML = "Last updated " + document.lastModified;
-  
-  document.getElementById("lastModified").innerHTML= year;
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let d = new Date();
+
+let myDate =
+  dayNames[d.getDay()] +
+  ", " +
+  monthNames[d.getMonth()] +
+  " " +
+  d.getDate() +
+  " " +
+  d.getFullYear();
+
+let year = d.getFullYear();
+
+document.querySelector("p").innerHTML = "Last updated " + document.lastModified;
+
+document.getElementById("lastModified").innerHTML = year;
