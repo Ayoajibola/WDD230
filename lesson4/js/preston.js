c1825e9dcd26211a5a439e8f4a139fbb80f5a566
ayoajibola <aji21001@byui.edu>
2021-05-17 23:54:11 +0100

    innital commit

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -1,4 +1,4 @@
 function toggleMenu(){
-    console.log(document.getElementById("primaryNav").classList);   
-    document.getElementById("primaryNav").classList.toggle("hide");
+   
+   document.getElementById("primaryNav").classList.toggle("hide");
 }
