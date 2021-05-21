a048d95f328bdb36f632cb16783e90cf742f3ee3
ayoajibola <aji21001@byui.edu>
2021-05-21 16:16:38 +0100

    innitial commit

---
+++ lesson5/js/bom.js
@@ -0,0 +1,10 @@
+document.getElementsByTagName("button").addEventListener("click", () => {
+  const input = [];
+  const button = [];
+  const output = [];
+  const input = document.querySelector("#favchap");
+  const button = document.querySelector("button");
+  const output = document.querySelector(".list");
+});
+
+let li = document.createElement('____');
