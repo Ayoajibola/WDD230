386333d7b7720e455bf0e05b471cc26b4f5ef5f4
ayoajibola <aji21001@byui.edu>
2021-05-26 21:18:52 +0100

    innitial update

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -1,10 +1,27 @@
-document.getElementsByTagName("button").addEventListener("click", () => {
-  const input = [];
-  const button = [];
-  const output = [];
-  const input = document.querySelector("#favchap");
-  const button = document.querySelector("button");
-  const output = document.querySelector(".list");
-});
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const output = document.querySelector('.list');
 
-let li = document.createElement('____');
+button.addEventListener("click", () => {
+  if (input.value != '') {
+    //Create the elements provided by user in the list.
+    let li = document.createElement('li');
+    let deletebutton = document.createElement('button');
+
+    //Change the value textContent.
+    li.textContent = input.value;
+    deletebutton.textContent = '❌';
+
+    li.append(deletebutton);
+    output.append(li);
+
+    deletebutton.addEventListener('click', function () {
+      // this line of code should use the removeChild() (Links to an external site.) method
+      output.removeChild(li);
+      input.focus();
+    });
+
+    input.value = '';
+    input.focus;
+  }
+});
