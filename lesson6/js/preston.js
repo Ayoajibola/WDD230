7cade4b7977c32a9c4730b431bf1128549977420
ayoajibola <aji21001@byui.edu>
2021-06-01 14:16:46 +0100

    innitial commit

--- lesson6/js/preston.js
+++ lesson6/js/preston.js
@@ -39,43 +39,22 @@ let myDate =
 
 let year = d.getFullYear();
 
-
-document.getElementById("currentDate").innerHTML = "Last updated " + document.lastModified;
+document.getElementById("currentDate").innerHTML =
+  "Last updated " + document.lastModified;
 
 document.getElementById("lastModified").innerHTML = year;
 
+let weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
-/* const aside = document.querySelector("#fiday-only");
-
-if(dayNames[d.getDay()] = Tuesday){
-        //Create the elements to display Friday message.
-      let aside = document.createElement("aside");
-      
-      //Display the appropriate message in the element.
-      aside.textContent = "Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-         
-    }*/
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   display: block;
- }
+let fridy = weekday[d.getDay()];
+if (fridy == weekday[2]) {
+  document.getElementById("friday-only").innerHTML =
+    "Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.";
 }
