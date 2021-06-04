29be508dfa374a88dcf31c3462507a3424d4dbd5
ayoajibola <aji21001@byui.edu>
2021-06-04 21:53:04 +0100

    innitial commit

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -4,29 +4,35 @@ let imagesToLoad = document.querySelectorAll("img[data-src]");
 
 //Replace the path to the image.
 
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
+const loadImages =
+  ((image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
 
-  //Remove the 'data-src' attribute after loading.
-  image.onload = () => {
-    image.removeAttribute("data-src");
-  };
+    //Remove the 'data-src' attribute after loading.
+    image.onload = () => {
+      image.removeAttribute("data-src");
+    };
+  },
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  }));
+
+const imgOptions = {
+  rootMargin: "0px 0px 50px 0px",
+  threshold: 1,
 };
-
-imagesToLoad.forEach((img) => {
-  loadImages(img);
-});
 
 //check for browser support, then proceed.
 if ("IntersectionObserver" in window) {
-  const observer = new IntersectionObserver((items, observer) => {
+  const observer = new IntersectionObserver((items) => {
     items.forEach((item) => {
       if (item.isIntersecting) {
         loadImages(item.target);
         observer.unobserve(item.target);
       }
     });
-  });
+  }, imgOptions);
+
   imagesToLoad.forEach((img) => {
     observer.observe(img);
   });
