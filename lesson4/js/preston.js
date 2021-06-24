a6a303754e2d931a4162efdf153e69d54af66e68
ayoajibola <aji21001@byui.edu>
2021-06-24 21:00:59 +0100

    innitial commit

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -2,6 +2,46 @@ function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
 
+let dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
+let d = new Date();
+
+let myDate =
+  dayNames[d.getDay()] +
+  ", " +
+  monthNames[d.getMonth()] +
+  " " +
+  d.getDate() +
+  " " +
+  d.getFullYear();
+
+  let year = d.getFullYear();
+
+
+document.getElementById("currentDate").innerHTML = "Last updated " + document.lastModified;
+
+document.getElementById("lastModified").innerHTML= year;
 
 
