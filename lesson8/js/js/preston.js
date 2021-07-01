ad117ca3def3551075c6a1eba0483e751db9e449
ayoajibola <aji21001@byui.edu>
2021-07-01 09:30:25 +0100

    commit

---
+++ lesson8/js/js/preston.js
@@ -0,0 +1,59 @@
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+let dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let d = new Date();
+
+let myDate =
+  dayNames[d.getDay()] +
+  ", " +
+  monthNames[d.getMonth()] +
+  " " +
+  d.getDate() +
+  " " +
+  d.getFullYear();
+
+let year = d.getFullYear();
+
+document.getElementById("currentDate").innerHTML =
+  "Last updated " + document.lastModified;
+
+document.getElementById("lastModified").innerHTML = year;
+
+let weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+let pancakeDay = weekday[d.getDay()];
+if (pancakeDay == weekday[5]) {
+  document.getElementById("friday-only").innerHTML = "Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.";
+}
