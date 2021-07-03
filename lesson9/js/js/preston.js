1210eca82830ec11f32651d116a81bbb77e5b35a
ayoajibola <aji21001@byui.edu>
2021-07-03 23:11:39 +0100

    commit

--- lesson9/js/js/preston.js
+++ lesson9/js/js/preston.js
@@ -57,3 +57,62 @@ let pancakeDay = weekday[d.getDay()];
 if (pancakeDay == weekday[5]) {
   document.getElementById("friday-only").innerHTML = "Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.";
 }
+
+
+
+const requestURL =
+  "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const towns = jsonObject["towns"];
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+
+   
+
+      for (let i = 0; i < 3; i++) {
+     
+        let card = document.createElement("section");
+        let h2 = document.createElement("h2");
+        let p = document.createElement("p");
+        let p2 = document.createElement("p");
+        let p3 = document.createElement("p");
+        let p4 = document.createElement("p");
+        let image = document.createElement("img");
+  
+        image.setAttribute("src", towns[i].photo);
+  
+        image.setAttribute(
+          "alt",
+          towns[i].name
+        );
+  
+        h2.textContent = towns[i].name;
+  
+        card.appendChild(h2);
+  
+        p.textContent = "Name of Town: " + towns[i].name;
+        card.appendChild(p);
+  
+        p2.textContent = towns[i].motto;
+        card.appendChild(p2);
+  
+        p3.textContent ="Current population: " + towns[i].currentPopulation;
+        card.appendChild(p3);
+  
+        p4.textContent ="Average rainfall: " + towns[i].averageRainfall;
+        card.appendChild(p4);
+  
+  
+  
+        card.appendChild(image);
+  
+        document.querySelector("div.cards").appendChild(card);
+      
+      }
+    });
+  
+      
